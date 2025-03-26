b879bbc669e1ed3b7f573d75718d973141746a58
kamyar <54872856+noakishere@users.noreply.github.com>
2025-03-26 09:45:49 -0400

    portal bug fix

--- Assets/_Scripts/Portals/PortalBehaviour.cs
+++ Assets/_Scripts/Portals/PortalBehaviour.cs
@@ -4,14 +4,52 @@ using UnityEngine;
 
 public class PortalBehaviour : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     [SerializeField] private Transform targetTransform;
+
+    public Transform receiver;
+    private bool playerIsOverlapping = false;
+
+    private void LateUpdate()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            // If this is true: The player has moved across the portal
+            if (dotProduct < 0f)
+            {
+                // Teleport him!
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                //rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            Debug.Log("HELLO");
-            other.transform.position = targetTransform.position;
+            playerIsOverlapping = true;
+            Debug.Log($"Enter {gameObject}");
+            //other.transform.position = targetTransform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+            Debug.Log($"Exit from {gameObject}");
+            //other.transform.position = targetTransform.position;
         }
     }
 }
