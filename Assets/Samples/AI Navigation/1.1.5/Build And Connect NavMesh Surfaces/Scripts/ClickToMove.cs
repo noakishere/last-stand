db592af2bccbaafe2bbce3fcbecd400770997292
kamyar <54872856+noakishere@users.noreply.github.com>
2025-04-01 20:03:22 -0400

    basic enemy following player

--- Assets/Samples/AI Navigation/1.1.5/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
+++ Assets/Samples/AI Navigation/1.1.5/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
@@ -11,20 +11,28 @@ namespace Unity.AI.Navigation.Samples
     {
         NavMeshAgent m_Agent;
         RaycastHit m_HitInfo = new RaycastHit();
-    
+
+        public Transform player;
+        
         void Start()
         {
+            //playerPos = player.position;
             m_Agent = GetComponent<NavMeshAgent>();
+            
         }
     
         void Update()
         {
-            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+            if (transform.position != player.position)
             {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
-                    m_Agent.destination = m_HitInfo.point;
+                m_Agent.destination = player.position;
             }
+            //if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+            //{
+            //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //    if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+            //        m_Agent.destination = m_HitInfo.point;
+            //}
         }
     }
 }
